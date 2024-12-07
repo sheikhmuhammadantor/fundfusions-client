810af896f35993f2ad28c35ca15b5c0ce3b752f8
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-07 20:32:36 +0600

    Remove Some npm dependencies and implement React-hot-toast properly.

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
+import { Toaster } from "react-hot-toast"
 
 function MainLayout() {
     return (
         <>
+            <Toaster position="bottom-center" reverseOrder={true} />
             <section className="mt-2">
                 <Navbar />
             </section>
